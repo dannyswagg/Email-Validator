a260b4ba1032d7357d4c858a70121d057996d41c
Ugiomoh Daniel <ugiomohd@yahoo.com>
2022-04-23 17:28:06 +0100

    commited

---
+++ script.js
@@ -0,0 +1,16 @@
+const err = document.querySelector("error");
+
+function getInput() {
+  let val = document.querySelector("input").value;
+  let formatRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  document.getElementById("p22").value = "";
+  if (formatRegex.test(val)) {
+    document.getElementById("p22").innerText = "Thank you for subscribing!";
+  } else {
+    document.getElementById("p22").innerText = "Please Enter a Valid Email";
+    document.getElementById("error").style.display = "inline";
+  }
+  setTimeout(function () {
+    location.reload();
+  }, 2000);
+}
